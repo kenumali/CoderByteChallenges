09b33fc7bc18d9437b706797e36f93d66fd0e472
kenumali <kenneth.n.umali@gmail.com>
2017-01-03 14:55:28 +0000

    Create FirstFactorial.java

---
+++ easy/FirstFactorial.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {  
+  public static int FirstFactorial(int num) { 
+    int factorial = 1;
+    for(int i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    return factorial;
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(FirstFactorial(s.nextLine())); 
+  }   
+}
